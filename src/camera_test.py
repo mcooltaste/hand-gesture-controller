f22c8eefdb59250244954ecda64ca04b24094f26
adesouza2006 <adesouza@vt.edu>
2026-07-23 22:39:23 -0400

    Basic Video Capture

---
+++ src/camera_test.py
@@ -0,0 +1,23 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open video stream.")
+    exit()
+
+print ("Webcam opened. Press 'q' to quit.")
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Error: Could not read frame.")
+        break
+
+    cv2.imshow('Live Webcam Feed', frame)
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
